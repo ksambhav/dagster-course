14b2c67b56f28890820ba6f035e86ed3ce34546c
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2024-08-14 21:46:29 +0530

    Multiple code location support

--- another_code_location/__init__.py
+++ another_code_location/__init__.py
@@ -1,9 +1,11 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_package_module
 
 from another_code_location.assets import orders
 
-orders_assert = load_assets_from_modules([orders], group_name="order_group")
+# orders_asset = load_assets_from_modules([orders], group_name="order_group")
+
+orders_asset = load_assets_from_package_module(package_module=assets)
 
 defs = Definitions(
-    assets=[*orders_assert]
+    assets=[*orders_asset]
 )
